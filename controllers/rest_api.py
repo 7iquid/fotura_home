cad0abd074bdf83d45170ea9da1964824ed39ed4
tavs <82376053+7iquid@users.noreply.github.com>
2021-07-22 01:23:34 +0800

    adding api to ajax to api

--- controllers/rest_api.py
+++ controllers/rest_api.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, Response, request, jsonify,render_template, session
+from flask import Flask, Blueprint, Response, request, jsonify,render_template, session, flash
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from models.Baracode_user_DB import Barcode_table
 from models.Baracode_user_DB import User
@@ -27,27 +27,61 @@ class User_Db(Resource):
 							})
 			user_list.append(data_dic)
 		return jsonify(user_list)
+api.add_resource(User_Db, "/api")
+
+
 
 class Barcode_Db(Resource):
-	def get(self):
-		data_dic = {}
-		user_list = []
-
-		data = Barcode_table.query.all()
-
-		for i in data:
-			img_ko =base64.b64encode(i.img_o)
-			img_ko = img_ko.decode("UTF-8")
-			data_dic =({
-							"id":i.id,
-							"part_no":i.part_no,
-							"by" : i.by,
-							"date_entry" : i.date_entry,
-							"img_ko": img_ko,
-							"mimetype_db": i.mimetype_db
+	def get(self,query_key = None,query_value = None):
+		if query_value:
+			data = Barcode_table.query.filter_by(barcode_id=query_value).first()
+			if data:
+				if data.prod_pic:
+					prod_pic =base64.b64encode(data.prod_pic)
+					data.prod_pic = prod_pic.decode("UTF-8")
+				data_dic =({
+					'id': data.id,
+		            'barcode_id': data.barcode_id, 
+		            'item_name': data.item_name,
+		            'brand_name' : data.brand_name,
+		            'part_no': data.part_no,
+		            'serila_no': data.serila_no,
+		            'description': data.description,
+		            'remarks': data.remarks,
+		            'prod_pic': data.prod_pic,
+		            'mimetype_db': data.mimetype_db,
+		            'date_added': data.date_added,
+		            'user_id': data.user_id
 							})
-			user_list.append(data_dic)
-		return jsonify(user_list)
+				return jsonify(data_dic)
+			flash('Item not exist', category='error')	
+			return jsonify({"error":"query not found"})
+		else:
+			Barcode_table.query.all()
+			for i in data:
+				if i.prod_pic:
+					prod_pic =base64.b64encode(i.prod_pic)
+					prod_pic = prod_pic.decode("UTF-8")
+				data_dic =({
+							'id': i.id,
+				            'barcode_id': i.barcode_id, 
+				            'item_name': i.item_name,
+				            'brand_name' : i.data.brand_name,
+				            'part_no': i.part_no,
+				            'serila_no': i.serila_no,
+				            'description': i.description,
+				            'remarks': i.remarks,
+				            'prod_pic': i.prod_pic,
+				            'mimetype_db': i.mimetype_db,
+				            'date_added': i.date_added,
+				            'user_id': i.user_id
+								})
+				user_list.append(data_dic)
+			return jsonify(user_list)
+
+api.add_resource(Barcode_Db, "/api_barcode/<string:query_key>/<string:query_value>")
+
+
 
 class test1(Resource):
 	def get(self):
@@ -85,7 +119,7 @@ class test2(Resource):
 		return jsonify(user_list)
 
 
-api.add_resource(User_Db, "/api")
-api.add_resource(Barcode_Db, "/api_barcode")
+
+
 api.add_resource(test1, "/mutithread_test1")
 api.add_resource(test2, "/mutithread_test2")
--- static/js_scrip/api_ajax.js
+++ static/js_scrip/api_ajax.js
@@ -1,17 +1,51 @@
-// var btn = document.getElementById(Search)
+$(document).ready(function() {
+    // get or read with arg
+    $('#barcode_read').on('click', function() {
+        // console.log("set 2 ok");
+        // console.log($('#barcode_id').val())
+        let query_key = "barcode_id";
+        let query_value = $('#barcode_id').val();
+        let url_ko = `http://127.0.0.1:5000/api_barcode/${query_key}/${query_value}`;
+        //       console.log(url_ko)
+        // console.log(`http://127.0.0.1:5000/api_barcode/${query_key}/${query_value}`);
+        $.ajax({
+            data: {
+                barcode_id: $('#barcode_id').val(),
+            },
+            type: 'GET',
+            url: url_ko
+        }).done(function(data) {
+            console.log(data.barcode_id);
+            console.log(data.id);
+            console.log(data.barcode_id);
+            console.log(data.item_name);
+            console.log(data.part_no);
+            console.log(data.serila_no);
+            console.log(data.description);
+            console.log(data.remarks);
+            console.log(data.prod_pic);
+            console.log(data.mimetype_db);
+            console.log(data.date_added);
+            console.log(data.user_id)
 
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhr = new XMLHttpRequest();
+            $("#item_name").val(data.item_name);
+            $("#brand_name").val(data.brand_name);
+            $("#part_no").val(data.part_no);
+            $("#serila_no").val(data.serila_no);
+            $("#description").val(data.description);
+            $("#remarks").val(data.remarks);
+            $("#prod_pic").val(data.prod_pic);
 
-var ourRequest = new XMLHttpRequest();
-ourRequest.open("GET", "http://127.0.0.1:5000/api");
-ourRequest.onload= function(){
-	var ourData = JSON.parse(ourRequest.responseText);
-	var name = ourData[1].name;
-	console.log(ourData[1]);
-	console.log(name)
-}
-ourRequest.send()
+        });
 
-var title ="java at tinapay"
-console.log(title);
+        //         event.preventDefault();
+
+    });
+
+
+
+
+
+
+
+});
