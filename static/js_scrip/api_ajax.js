d827ce433d6c6219442fb9b0b030aef54e619f15
tavs <82376053+7iquid@users.noreply.github.com>
2021-07-25 23:39:27 +0800

    1

--- static/js_scrip/api_ajax.js
+++ static/js_scrip/api_ajax.js
@@ -6,48 +6,52 @@ $(document).ready(function() {
         data_2 = $("#barcode_id").serializeJSON();
         console.log(data_2)
         const settings = {
-                url: url_ko,
-                // dataType:"headers",
-                 data :{"cake":data_2.barcode_id},
-                method: "GET",
-                
-            };
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-        $("#item_name").val(response.item_name);
+            "url": url_ko,
+            "data": { "cake": data_2.barcode_id },
+            "method": "GET",
+            error: function(xhr, ajaxOptions, thrownError) {
+                if (xhr.status == 404) {
+                    alert(thrownError);
+                }
+            },
+        };
 
-        // alert("good");
-        // Object.entries(response).forEach(([k, v], i) => console.log(i, k, v))
-        var key = "#";
-        Object.entries(response).forEach(([k, v], i)=> $("#"+k).val(v));
+        $.ajax(settings).done(function(response) {
+            console.log(response);
+            $("#item_name").val(response.item_name);
+
+            // alert("good");
+            // Object.entries(response).forEach(([k, v], i) => console.log(i, k, v))
+
+            Object.entries(response).forEach(([k, v], i) => $("#" + k).val(v));
             // let value = v;
 
-            
+
             // console.log(i, k, v))
 
 
-        // console.log(response.item_name);
+            // console.log(response.item_name);
         });
-
     });
+
     $('#barcode_create').on('click', function() {
 
         let url_ko = 'http://127.0.0.1:5000/api_barcode/tamina';
         data_2 = $('form').serializeJSON();
         const settings = {
-                url: url_ko,
-                dataType:"json",
-                 data : JSON.stringify(data_2),
-                method: "PUT",
-                headers: {
-                    "x-api-key": "API_KEY",
-                    "Content-type": "application/json"
-                }
-                
-            };
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-    
+            url: url_ko,
+            dataType: "json",
+            data: JSON.stringify(data_2),
+            method: "PUT",
+            headers: {
+                "x-api-key": "API_KEY",
+                "Content-type": "application/json"
+            }
+
+        };
+        $.ajax(settings).done(function(response) {
+            console.log(response);
+
         });
 
     });
